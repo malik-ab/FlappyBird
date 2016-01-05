4c279cb834bedaf274bb66bbbf77dd724525a548
Malik Allah Bakhsh <prince_malikab@hotmail.com>
2016-01-05 12:16:35 +0500

    Initial commit

---
+++ android/src/com/lysaan/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.lysaan.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.lysaan.game.FlappyDemo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyDemo(), config);
+	}
+}
